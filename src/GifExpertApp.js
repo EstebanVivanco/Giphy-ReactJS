d0a776f42e72662ece6a9d53722dc0af30182e94
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2022-03-25 21:37:35 -0300

    Buenas

---
+++ src/GifExpertApp.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch Man','Samurai X','Kimetsu No Yaiba'])
+
+//   const handleAdd = () => {
+
+//     setCategories([...categories, 'Hunter X Hunter']);
+
+//     }
+
+  return (
+    <div>
+
+        <h2>Gif Expert App</h2>
+        <AddCategory setCategories={setCategories} ></AddCategory>
+        <hr/>
+
+        <ol>
+            { 
+                categories.map( (category) => {
+                    return <li key={category}> {category} </li>
+                })
+            }
+        </ol>
+        
+
+    </div>
+  )
+}
